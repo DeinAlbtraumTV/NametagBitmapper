44b00c1e40621cee42e979ecb93601613baa085b
Max <max@dyren.de>
2025-05-06 15:41:48 +0200

    add some additional buttons (#3)
    * add some more function buttons
    
    * fix: https://github.com/DeinAlbtraumTV/NametagBitmapper/pull/3#issuecomment-2567091655
    
    * minimal formatting ;)

--- create_bmp.py
+++ create_bmp.py
@@ -57,12 +57,12 @@ class BMPCreator(tk.Frame):
             self.redraw()
 
     def redraw(self):
-        self.frame_counter.config(text=f"{self.frame + 1}/{len(self.frames)}")
+        self.frame_counter.config(text=f"{self.frame + 1}/{len(self.frames)}") #spalte / zeile
         for i, buttons in enumerate(self.buttons):
             for j, button in enumerate(buttons):
                 button.config(bg=("white" if self.frames[self.frame][i][j] else "black"),
                               fg=("black" if self.frames[self.frame][i][j] else "white"))
-
+                
     def load(self):
         with open(storage_dir.joinpath(self.filename)) as f:
             self.frames = json.load(f)
@@ -86,15 +86,29 @@ class BMPCreator(tk.Frame):
 
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
-        # Object var setup
+        
+        # top row
         self.import_button = None
-        self.save_button = None
-        self.frame_counter = None
         self.decr_button = None
-        self.incr_button = None
-        self.clone_button = None
+        self.incr_button = None       
+        
         self.clear_button = None
+        self.delete_button = None
         self.create_button = None
+        self.clone_button = None
+        
+        # bottom row
+        self.shift_frame_left_button = None
+        self.shift_frame_right_button = None
+        self.shift_frame_up_button = None
+        self.shift_frame_down_button = None
+        
+        self.frame_counter = None
+        self.save_button = None
+        
+        # close button
+        self.close_button = None
+        
         self.img = tk.PhotoImage(width=1, height=1)
         self.buttons = None
         self.frame = 0
@@ -124,34 +138,50 @@ class BMPCreator(tk.Frame):
             self.buttons.append([])
             for j in range(11):
                 self.buttons[i].append(tk.Button(self, width=37, height=37, text=f"{i+1}/{j+1}", borderwidth=1, highlightthickness=0, padx=0, pady=0, image=self.img, compound="center", command=(lambda x=i, y=j: self.toggle_pxl(x, y)), bg=("white" if self.frames[self.frame][i][j] else "black"), fg=("black" if self.frames[self.frame][i][j] else "white")))
-                self.buttons[i][j].grid(row=j, column=i)
+                self.buttons[i][j].grid(row=j+1, column=i)
         self.clone_button = tk.Button(self, text="Duplicate frame to end", command=self.clone_frame)
-        self.clone_button.grid(row=44, column=36, columnspan=8, sticky="news")
+        self.clone_button.grid(row=45, column=36, columnspan=8, sticky="news")
         
         self.clear_button = tk.Button(self, text="Clear frame", command=self.clear_frame)
-        self.clear_button.grid(row=44, column=20, columnspan=4, sticky="news")
+        self.clear_button.grid(row=45, column=20, columnspan=4, sticky="news")
         
-        self.clear_button = tk.Button(self, text="Delete frame", command=self.del_frame)
-        self.clear_button.grid(row=44, column=24, columnspan=4, sticky="news")
+        self.delete_button = tk.Button(self, text="Delete frame", command=self.del_frame)
+        self.delete_button.grid(row=45, column=24, columnspan=4, sticky="news")
         
         self.create_button = tk.Button(self, text="Create new frame", command=self.create_frame)
-        self.create_button.grid(row=44, column=28, columnspan=8, sticky="news")
+        self.create_button.grid(row=45, column=28, columnspan=8, sticky="news")
         
         self.incr_button = tk.Button(self, text="++", command=self.incr_frame)
-        self.incr_button.grid(row=44, column=12, columnspan=8, sticky="news")
+        self.incr_button.grid(row=45, column=12, columnspan=8, sticky="news")
         
         self.decr_button = tk.Button(self, text="--", command=self.decr_frame)
-        self.decr_button.grid(row=44, column=4, columnspan=8, sticky="news")
+        self.decr_button.grid(row=45, column=4, columnspan=8, sticky="news")
         
         #ToDo add a Disclaimer that this overwrites the current frame
         self.import_button = tk.Button(self, text="Import from BMP", command=(lambda: self.load_frame_from_bmp(tk.filedialog.askopenfilename())))
-        self.import_button.grid(row=44, column=0, columnspan=4, sticky="news")
+        self.import_button.grid(row=45, column=0, columnspan=4, sticky="news")
+        
+        self.shift_frame_left_button = tk.Button(self, text="⇦ (h)", command=self.shift_frame_left) #h
+        self.shift_frame_left_button.grid(row=46, column=0, columnspan=4, sticky="news")
+        
+        self.shift_frame_down_button = tk.Button(self, text="⇩ (j)", command=self.shift_frame_down) # j
+        self.shift_frame_down_button.grid(row=46, column=4, columnspan=4, sticky="news")
+        
+        self.shift_frame_up_button = tk.Button(self, text="⇧ (k)", command=self.shift_frame_up) #k
+        self.shift_frame_up_button.grid(row=46, column=8, columnspan=4, sticky="news")
+        
+        self.shift_frame_right_button = tk.Button(self, text="⇨ (l)", command=self.shift_frame_right) #l
+        self.shift_frame_right_button.grid(row=46, column=12, columnspan=4, sticky="news")
         
         self.frame_counter = tk.Label(self, text=f"{self.frame + 1}/{len(self.frames)}", bg="black", fg="white")
-        self.frame_counter.grid(row=45, column=0, columnspan=40, sticky="news")
+        self.frame_counter.grid(row=46, column=16, columnspan=40-12, sticky="news") #todo columspan
         
         self.save_button = tk.Button(self, text="Save", command=self.save)
-        self.save_button.grid(row=45, column=40, columnspan=4, sticky="news")
+        self.save_button.grid(row=46, column=40, columnspan=4, sticky="news")
+        
+        self.close_button = tk.Button(self, text="Close", command=self.quit, bg="red", fg="white")
+        self.close_button.grid(row=0, column=42, columnspan=3, sticky="news")        
+        
 
     def setup_frame(self):
         self.frames.append([])
@@ -172,10 +202,29 @@ class BMPCreator(tk.Frame):
                 self.frames[self.frame][i][j] = pixels[i, j] == 255
         self.redraw()
         
-
+    def shift_frame_left(self):
+        self.frames[self.frame] = self.frames[self.frame][1:] + [self.frames[self.frame][0]]
+        self.redraw()
+        
+    def shift_frame_right(self):
+        self.frames[self.frame] = [self.frames[self.frame][-1]] + self.frames[self.frame][:-1]
+        self.redraw()
+        
+    def shift_frame_up(self):
+        self.frames[self.frame] = [row[1:] + [False] for row in self.frames[self.frame]]
+        self.redraw()
+        
+    def shift_frame_down(self):
+        self.frames[self.frame] = [[False] + row[:-1] for row in self.frames[self.frame]]
+        self.redraw()
+    
+        
+        
 
 if __name__ == "__main__":
     args = sys.argv[1:]
     root = tk.Tk()
-    BMPCreator(root).pack(fill="both", expand=True)
+    window = BMPCreator(root)
+    window.pack(fill="both", expand=True)
+    
     root.mainloop()
